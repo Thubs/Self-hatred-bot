86dab01cc851150cf86adb9ca6a88931c34078b6
Thubs <hughcookpalmeh@gmail.com>
2022-03-26 19:47:44 +1100

    Add files via upload

---
+++ selfhatredbot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import discordapi
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Fuck you!')
+
+client.run(os.getenv('TOKEN'))
